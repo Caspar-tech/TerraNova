b1f7e95556b5e700dd6e1c3952e1d3dc36b6f9c2
Caspar <caspar.lombaers@gmail.com>
2021-08-01 16:22:20 +0200

    Initial commit

---
+++ Game/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views as user_views
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+        path('', user_views.main, name='main'),
+        path('login/', auth_views.LoginView.as_view(template_name='Game/login.html'), name='login'),
+        path('logout/', auth_views.LogoutView.as_view(template_name='Game/logout.html'), name='logout'),
+]
---
+++ Game/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, redirect
+from .forms import UserRegisterForm 
+#from .models import <name database>
+
+# Main-page render funtcion
+def main(request):
+    context = {
+         "A": "B"        
+    }
+    return render(request, 'Game/main.html', context)
+
+#Register-page render function
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('main')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'Game/register.html', {'form': form})
